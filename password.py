e7b241a9f3a341bcc1267441f7abb86b60c1ac68
{mwikiaBundi1} <{mwikiam@gmail.com}>
2020-04-18 18:35:01 +0300

    Add create account method for creating user accounts

--- password.py
+++ password.py
@@ -1,13 +1,13 @@
-# class Credentials:
-#     '''
-#     Class that creates accounts and authenticates the users
-#     '''
-#     users_list = []
+class Credentials:
+    '''
+    Class that creates accounts and authenticates the users
+    '''
+    users_list = []
 
-    def __init__(self, recognise, username, password):
-        '''
-        Initalizing the variables
-        '''
+    # def __init__(self, recognise, username, password):
+    #     '''
+    #     Initalizing the variables
+    #     '''
         self.recognise = recognise
         self.username = username
         self.password = password
