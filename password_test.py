766436772a3533423bfa389c336f927fa74d99c6
{mwikiaBundi1} <{mwikiam@gmail.com}>
2020-04-18 19:09:50 +0300

    Set up structure for our tests

--- password_test.py
+++ password_test.py
@@ -7,11 +7,11 @@ class TestCredentials(unittest.TestCase):
     Test class that defines the test cases for creating and authenticating credentials
     '''
 
-    def setUp(self):
-        '''
-        Setting up the structure before each test
-        '''
-        self.new_user = Credentials(1, "martin", "mwikia")
+    # def setUp(self):
+    #     '''
+    #     Setting up the structure before each test
+    #     '''
+    #     self.new_user = Credentials(1, "martin", "mwikia")
 
 
     def tearDown(self):
