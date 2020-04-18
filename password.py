45e92ea79c2d45fcae07b8c5a785c0bf01582f97
{mwikiaBundi1} <{mwikiam@gmail.com}>
2020-04-18 18:31:53 +0300

    create credentials class for account and user authentication

--- password.py
+++ password.py
@@ -1,8 +1,8 @@
-class Credentials:
-    '''
-    Class that creates accounts and authenticates the users
-    '''
-    users_list = []
+# class Credentials:
+#     '''
+#     Class that creates accounts and authenticates the users
+#     '''
+#     users_list = []
 
     def __init__(self, recognise, username, password):
         '''
